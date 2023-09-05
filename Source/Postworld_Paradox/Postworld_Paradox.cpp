3a35fb5add8657da1f9fd4019274d068cec9731c
Lietziboy <michael.lietz@gmx.at>
2023-09-05 21:26:48 +0200

    Added basis for player stats

---
+++ Source/Postworld_Paradox/Postworld_Paradox.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Postworld_Paradox.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Postworld_Paradox, "Postworld_Paradox" );
---
+++ Source/Postworld_Paradox/Private/PostworldParadox.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PostworldParadox.h"
+
+PostworldParadox::PostworldParadox()
+{
+}
+
+PostworldParadox::~PostworldParadox()
+{
+}
